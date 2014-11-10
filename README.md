687e595cd676e7473df6bfab33ac3fe519ab1c89
Antonio Augusto <antonioabritto@gmail.com>
2014-11-10 11:38:56 -0200

    structure of new project

---
+++ README.md
@@ -0,0 +1,14 @@
+The Odin Project
+================
+
+This is my solution for the [Rails Project](http://www.theodinproject.com/web-development-101/ruby-on-rails?ref=lnav).
+
+To create the app I preferred using rspec rather than Test::Unit, so instead of creating with `rails new blogger`
+I did the steps below to get the project started with RSpec:
+
+	1. `$ rails new blogger -T`
+	2. `gem 'rspec-rails' #added to Gemfile`
+	3. `$ bundle install`
+	4. `$ rails generate rspec:install`
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blogger::Application.config.secret_key_base = '29598890f0458e37336b78ca743f7f015afc073165a9d6b4e6a2535c76968ddee94d5386ae1c198a30dd4336a0fb01a367f8ba7f23241ec1e06da9d6c24be0de'
